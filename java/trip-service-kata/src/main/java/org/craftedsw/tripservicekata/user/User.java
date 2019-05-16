814c885fcdb665f350e81c2506aaec99d8d8be62
Gregor Riegler <gregor.riegler@ebcont.com>
2019-05-16 18:38:29 +0200

    adds test for when user not logged in

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/User.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/User.java
@@ -1,9 +1,9 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.craftedsw.tripservicekata.trip.Trip;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.craftedsw.tripservicekata.trip.Trip;
 
 public class User {
 
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,34 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceTest {
-	
+
+    @Test
+    void userNotLoggedIn() {
+        TripService tripService = new TestTripService(null);
+
+        Executable executable = () -> tripService.getTripsByUser(new User());
+
+        assertThrows(UserNotLoggedInException.class, executable);
+    }
+
+    public class TestTripService extends TripService {
+
+        private User loggedInUser;
+
+        public TestTripService(User loggedInUser) {
+        }
+
+        @Override
+        protected User loggedInUser() {
+            return loggedInUser;
+        }
+    }
+
 }
