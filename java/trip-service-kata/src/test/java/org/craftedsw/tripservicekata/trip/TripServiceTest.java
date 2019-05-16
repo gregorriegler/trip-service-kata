59d7c6af453b5319b111ea87da51a1fd88b60954
Gregor Riegler <gregor.riegler@ebcont.com>
2019-05-16 19:50:43 +0200

    TripService covered with tests

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -2,15 +2,29 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TripServiceTest {
 
+    private User loggedInUser;
+
+    @BeforeEach
+    void setUp() {
+        loggedInUser = new User();
+    }
+
     @Test
-    void userNotLoggedIn() {
+    void userNotLoggedIn_throwsException() {
         TripService tripService = new TestTripService(null);
 
         Executable executable = () -> tripService.getTripsByUser(new User());
@@ -18,16 +32,56 @@ public class TripServiceTest {
         assertThrows(UserNotLoggedInException.class, executable);
     }
 
+    @Test
+    void notFriends_emptyList() {
+        TripService tripService = new TestTripService(loggedInUser);
+
+        List<Trip> tripsByUser = tripService.getTripsByUser(new User());
+
+        assertThat(tripsByUser, is(empty()));
+    }
+
+    @Test
+    void hasOtherFriends_emptyList() {
+        User notFriend = new User();
+        notFriend.addFriend(new User());
+        TripService tripService = new TestTripService(loggedInUser);
+
+        List<Trip> tripsByUser = tripService.getTripsByUser(notFriend);
+
+        assertThat(tripsByUser, is(empty()));
+    }
+
+    @Test
+    void returnsTripOfFriend() {
+        User friendOfLoggedInUser = new User();
+        friendOfLoggedInUser.addFriend(loggedInUser);
+        Trip trip = new Trip();
+        friendOfLoggedInUser.addTrip(trip);
+
+        TripService tripService = new TestTripService(loggedInUser);
+
+        List<Trip> tripsByUser = tripService.getTripsByUser(friendOfLoggedInUser);
+
+        assertThat(tripsByUser, contains(trip));
+    }
+
     public class TestTripService extends TripService {
 
         private User loggedInUser;
 
         public TestTripService(User loggedInUser) {
+            this.loggedInUser = loggedInUser;
         }
 
         @Override
         protected User loggedInUser() {
             return loggedInUser;
+        }
+
+        @Override
+        protected List<Trip> tripsByUser(User user) {
+            return user.trips();
         }
     }
 
